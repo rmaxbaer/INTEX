7f8be84525f7a4974efd9ba6cc551a110ca15e9a
Brandon Jacobsen <brandonerikjacobsen@gmail.com>
2020-12-04 16:44:16 -0700

    initial models

--- applicant/models.py
+++ applicant/models.py
@@ -1,3 +1,29 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Skill(models.Model):
+
+    skill_name = models.CharField(max_length=50)
+    proficiency = models.IntegerField()
+
+    def __str__(self):
+        return self.skill_name
+
+class Applicant(User):
+
+    birth_date = models.DateField(auto_now=False, auto_now_add=False)
+    resume = models.FileField(upload_to=None, max_length=100)
+    skills = models.ManyToManyField(Skill)
+
+    def __str__(self):
+        return User.first_name + ' ' + User.last_name
+
+class Application(models.Model):
+
+    application_date = models.DateTimeField(auto_now=False, auto_now_add=False)
+    applicant = models.ForeignKey(Applicant, on_delete=models.CASCADE)
+    listing = models.ForeignKey('organization.Listing', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
--- organization/models.py
+++ organization/models.py
@@ -1,3 +1,49 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Organization(User):
+
+    company_name = models.CharField(max_length=50)
+    size = models.CharField(max_length=4)
+    sectors = models.CharField(max_length=4)
+    description = models.TextField()
+    date_founded = models.DateField(auto_now=False, auto_now_add=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Listing(models.Model):
+
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    job_title = models.CharField(max_length=50)
+    job_description = models.TextField()
+    contract_type = models.CharField(max_length=50)
+    compensation = models.CharField(max_length=50)
+    relocation_assistance = models.BooleanField(default=False)
+    skills = models.ManyToManyField('applicant.Skill')
+
+    def __str__(self):
+        return self.name
+
+
+class Position(models.Model):
+
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    location = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+class Offer(models.Model):
+
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+    applicant = models.ForeignKey('applicant.Applicant', on_delete=models.CASCADE)
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    deadline = models.DateTimeField(auto_now=False, auto_now_add=False)
+    start_date = models.DateField(auto_now=False, auto_now_add=False)
+    status = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
