2690501372c54301b0a29f5138ff85c31fa5ba22
Brandon Jacobsen <brandonerikjacobsen@gmail.com>
2020-12-08 15:16:03 -0700

    applicant list views

--- applicant/views.py
+++ applicant/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Applicant, Skill, Application
+from organization.models import Organization, Listing, Offer
 
 # Create your views here.
 def ProfileView(request, username):
@@ -8,7 +10,14 @@ def ProfileEditView(request, username):
     pass
 
 def ListingsView(request, username):
-    pass
+    applicant = Applicant.objects.filter(username=username)
+
+    context = {
+        'username':username,
+        'applicant':applicant,
+        'listings':Listing.objects.all()
+    }
+    return render(request, 'applicant/listings.html', context)
 
 def ListingView(request, username):
     pass
@@ -17,10 +26,25 @@ def ApplicationView(request, username):
     pass
 
 def ApplicationsView(request, username):
-    pass
+    applicant=Applicant.objects.filter(username=username)[0]
+
+    context = {
+        'username':username,
+        'applicant':applicant,
+        'applications':Application.objects.filter(applicant=applicant)
+    }
+    return render(request, 'applicant/applications.html', context)
 
 def OffersView(request, username):
-    pass
+    applicant=Applicant.objects.filter(username=username)[0]
+    applications = Application.objects.filter(applicant=applicant)
+
+    context = {
+        'username':username,
+        'applicant':applicant,
+        'offers':Offer.objects.filter(application__in=applications)
+    }
+    return render(request, 'applicant/offers.html', context)
 
 def OfferView(request, username):
     pass
@@ -29,4 +53,11 @@ def OrganizationView(request, username):
     pass
 
 def OrganizationsView(request, username):
-    pass
+    applicant=Applicant.objects.filter(username=username)[0]
+
+    context = {
+        'username':username,
+        'applicant':applicant,
+        'organizations':Organization.objects.all()
+    }
+    return render(request, 'applicant/organizations.html', context)
