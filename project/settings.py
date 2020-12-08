1533f634de27a449b742ce47dc48a3d1944925e4
Tyler <tylerelijahsmith15@gmail.com>
2020-12-07 21:52:39 -0700

    fixed my dumb mistake.

--- project/settings.py
+++ project/settings.py
@@ -58,7 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.runserver_nostatic', 
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'project.urls'
